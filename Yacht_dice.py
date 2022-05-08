1f9894c9bcf27144b7555c153baba7ea8d54b3e6
ARC0112 <hhhoon0112@gmail.com>
2022-05-08 21:50:47 +0900

    UI update

--- Yacht_dice.py
+++ Yacht_dice.py
@@ -1,5 +1,5 @@
-import rule
 import pygame
+import rule
 
 #색깔 세팅
 blue = (0, 0, 255)
@@ -8,28 +8,43 @@ green = (0, 255, 0)
 black = (0, 0, 0)
 white = (255, 255, 255)
 
+#이미지를 리스트로 정리
 di = ["dice1.png", "dice2.png", "dice3.png", "dice4.png", "dice5.png", "dice6.png"]
 
 #주사위 굴리기(랜덤 지정)
 rule.roll()
+rule.count()
 #화면에 나온 이미지와 주사위 눈이 같은지 출력해서 확인
-print(rule.dice)
 
 #pygame문법
 while True:
     pygame.init()
+    event = pygame.event.poll()
 
+    #화면 설정
     display = pygame.display.set_mode((1200, 800))
     display.fill(white)
+
+    #화면 이름 설정
+    pygame.display.set_caption("Yacht")
+
+    #rule에서 작성한 점수판 함수 실행
+    rule.score_board(display)
+    rule.score_load(display)
+
+
     #화면에 랜덤으로 뽑은 주사위 출력
+    #리스트내 주사위의 값이 1~6중 어느 것인지 인식 -> 그에 해당하는 이미지 출력
     for i in range(5):
         for j in range(1, 7):
             if rule.dice[i] == j:
                 image = pygame.image.load(di[j - 1])
-                display.blit(image, [240 + 150 * i, 400])
+                display.blit(image, [550 + 100 * i, 350])
 
     pygame.display.flip()
 
-
-
+    #화면 종료 인식 + 인식 시 종료
+    if event.type == pygame.QUIT:
+        pygame.quit()
+        break
 
--- rule.py
+++ rule.py
@@ -2,24 +2,33 @@
 
 import random
 import pygame
-import Yacht_dice
 
-dice = []
-dice_count = []
+#색깔 세팅
+blue = (0, 0, 255)
+red = (255, 0, 0)
+green = (0, 255, 0)
+black = (0, 0, 0)
+white = (255, 255, 255)
+
+
+dice = [0, 0, 0, 0, 0]
+dice_count = [0, 0, 0, 0, 0, 0]
 
 #주사위를 굴리는 함수
 def roll():
     for i in range(5):
-        dice.append(random.randint(1, 6))
+        dice[i]=random.randint(1, 6)
 
 #주사위에 나온 숫자들 각각의 개수를 측정
-#dice 리스트의 원소는 5개인데 dice_list 리스트의 원소는 6개 이므로 반복분으로 5개 + 코드 한줄로 만들었습니다.(더 나은 방법이 있으면 수정 부탁드려요!)
-def count():
-    for i in range(1, 6):
-        dice_count.append(dice.count(i))
-    dice_count.append(dice.count(6))
 
-#aces, deudces, threes, fours, fives, sixes 점수 계산
+
+def count():
+    for i in range(0, 6):
+        dice_count[i] = dice.count(i + 1)
+
+
+#aces, deuces, threes, fours, fives, sixes 점수 계산
+#함수 자체를 쓰지는 않지만 계산 방식 정리해두려고 놔뒀습니다.
 def calculate():
     aces = dice_count[0]
     deuces = dice_count[1] * 2
@@ -27,18 +36,15 @@ def calculate():
     fours = dice_count[3] * 4
     fives = dice_count[4] * 5
     sixes = dice_count[5] * 6
-    print("Aces:", aces)
-    print("Deuces:", deuces)
-    print("Threes:", threes)
-    print("Fours:", fours)
-    print("Fives:", fives)
-    print("Sixes:", sixes)
 
-#4 of kind는 같은 눈이 4개일때 5개의 눈의 숫자를 모두 더하므로 그냥 4개가 같으면 4 of a kind라고 판단
+
+#4 of kind는 같은 눈이 4개 이상일때 5개의 눈의 숫자를 모두 더하므로 그냥 4개가 같으면 4 of a kind라고 판단
 def four_of_a_kind():
     for i in range(6):
-        if dice_count[i] == 4:
-            print("4 of a kind!:", sum(dice))
+        if dice_count[i] >= 4:
+            return sum(dice)
+
+    return 0
 
 #if문으로 굴린 주사위에서 같은 눈이 각각 3개, 2개 있는지 검사하고 count 변수를 +1 모두 검사한 후에 count 변수의 값이 2면 Full House 판단
 def full_house():
@@ -50,7 +56,9 @@ def full_house():
         elif dice_count[i] == 2:
             count_2 += 1
     if count_3 == 1 and count_2 == 1:
-        print("Full House!:", sum(dice))
+        return sum(dice)
+
+    return 0
 
 #1~4까지의 숫자가 dice 리스트안에 들어있으면 count 증가 -> count == 4이면 small_striaght의 조건 완성: 1234 / (1, 2, 3, 4, 4의 경우도 커버 가능)
 #밑에 if문을 통해 2345, 3456의 경우를 커버(elif를 쓰면 위의 조건부터 체크, 그렇기 때문에 각각 if문을 써 각자 조건을 체크)
@@ -67,7 +75,9 @@ def small_straight():
         if i + 2 in dice:
             count3 += 1
     if count1 == 4 or count2 == 4 or count3 == 4:
-        print("small_straighat: 15")
+        return 15
+
+    return 0
 
 
 #먼저 서로 다른 숫자 5개가 나왔는지 확인 -> 이때 1 or 6이 0일경우 Large Straight는 성립하지 않으므로 이를 조건으로 판단
@@ -78,14 +88,135 @@ def large_straight():
             count += 1
     if count == 5:
         if dice_count[0] == 0:
-            print("Large Straight: 30")
+            return 30
         elif dice_count[5] == 0:
-            print("Large Straight: 30")
+            return 30
 
+    return 0
 
 #나온 주사위의 눈이 모두 같을 때 50점
-def yahtzee():
+def yacht():
     for i in range(6):
         if dice_count[i] == 5:
-            print("Yahtzee: 50")
+            return 50
+
+    return 0
+
+#점수판
+def score_board(display):
+    #기본틀 그리기
+    pygame.draw.rect(display, black, [50, 5, 400, 780], 1)
+
+    # 유저 칸 그리기
+    pygame.draw.line(display, black, [228, 30], [428, 30], 3)
+    pygame.draw.line(display, black, [428, 30], [428, 130], 3)
+    pygame.draw.line(display, black, [228, 30], [228, 130], 3)
+    pygame.draw.line(display, black, [328, 30], [328, 130], 3)
+
+    #위 점수판 그리기
+    pygame.draw.rect(display, black, [70, 130, 360, 305], 3)
+    pygame.draw.line(display, black, [70, 170], [429, 170], 3)
+    pygame.draw.line(display, black, [70, 210], [429, 210], 3)
+    pygame.draw.line(display, black, [70, 250], [429, 250], 3)
+    pygame.draw.line(display, black, [70, 290], [429, 290], 3)
+    pygame.draw.line(display, black, [70, 330], [429, 330], 3)
+    pygame.draw.line(display, black, [70, 370], [429, 370], 3)
+    pygame.draw.line(display, black, [328, 130], [328, 431], 3)
+    pygame.draw.line(display, black, [228, 130], [228, 431], 3)
+
+    #CHOICE 칸 만들기
+    pygame.draw.rect(display, black, [70, 457, 360, 40], 3)
+    pygame.draw.line(display, black, [328, 457], [328, 496], 3)
+    pygame.draw.line(display, black, [228, 457], [228, 496], 3)
+    pygame.draw.line(display, black, [223, 457], [223, 496], 3)
+
+
+    #아래 점수판 그리기
+    pygame.draw.rect(display, black, [70, 501, 360, 205], 3)
+    pygame.draw.line(display, black, [70, 541], [429, 541], 3)
+    pygame.draw.line(display, black, [70, 581], [429, 581], 3)
+    pygame.draw.line(display, black, [70, 621], [429, 621], 3)
+    pygame.draw.line(display, black, [70, 661], [429, 661], 3)
+    pygame.draw.line(display, black, [70, 661], [429, 661], 3)
+    pygame.draw.line(display, black, [328, 501], [328, 705], 3)
+    pygame.draw.line(display, black, [228, 501], [228, 705], 3)
+    pygame.draw.line(display, black, [223, 501], [223, 705], 3)
+
+
+    #TOTAL칸 그리기
+    pygame.draw.rect(display, black, [70, 710, 360, 70], 3)
+    pygame.draw.line(display, black, [328, 710], [328, 779], 3)
+    pygame.draw.line(display, black, [228, 710], [228, 779], 3)
+
+    #규칙 그리기
+    font = pygame.font.SysFont(None, 30)
+
+    aces = font.render('Aces', True, black)
+    deuces = font.render('Deuces', True, black)
+    threes = font.render('Threes', True, black)
+    fours = font.render('Fours', True, black)
+    fives = font.render('Fives', True, black)
+    sixes = font.render('Sixes', True, black)
+
+    display.blit(aces, (112, 143))
+    display.blit(deuces, (112, 183))
+    display.blit(threes, (112, 223))
+    display.blit(fours, (112, 263))
+    display.blit(fives, (112, 303))
+    display.blit(sixes, (112, 343))
+
+    choice = font.render('Choice', True, black)
+    fok = font.render('4 of a Kind', True, black)
+    fh = font.render('Full House', True, black)
+    s_straight = font.render('S. straight', True, black)
+    l_straight = font.render('L. Straight', True, black)
+    yacht = font.render('Yacht', True, black)
+
+    display.blit(choice, (112, 470))
+    display.blit(fok, (116, 514))
+    display.blit(fh, (112, 554))
+    display.blit(s_straight, (112, 594))
+    display.blit(l_straight, (112, 634))
+    display.blit(yacht, (112, 674))
+
+
+def score_load(display):
+    #폰트 설정
+    font = pygame.font.SysFont(None, 30)
+
+    #나중에 지울 임시 코드(그냥 리스트에 해당하는 숫자띄우려고 하는 것  +USER1 띄우기)
+    font2 = pygame.font.SysFont(None, 100)
+    num = font2.render(str(dice), True, black)
+    display.blit(num, (605, 250))
+
+    name = font.render('USER1', True, black)
+    display.blit(name, (245, 75))
+
+    #어떤 텍스르를 넣을지 설정
+
+    #Aces, Dueces와 같은 기본 규칙을 반복문으로 표시
+    for i in range(6):
+        text1 = font.render(str(dice_count[i] * (i + 1)), True, black)
+        if dice_count[i] * (i + 1) < 10:
+            display.blit(text1, (270, 143 + 40 * i))
+        else:
+            display.blit(text1, (265, 143 + 40 * i))
+
+    #두자리 수를 같은 위치에 띄우면 균형이 안맞는 것 같아서 if문 써서 살짝 조정
+    #Choice나 s_straight와 같은 규칙에 의한 점수를 반복문으로 표시
+
+    s_score = [sum(dice), four_of_a_kind(), full_house(), small_straight(), large_straight(), yacht()]
+    j = 0
+
+    for i in s_score:
+        text2 = font.render(str(i), True, black)
+        if i < 10:
+            display.blit(text2, (270, 470 + 40 * j))
+        else:
+            display.blit(text2, (265, 470 + 40 * j))
+        j += 1
+
+
+
+
 
